8c2cdd1597b514339e44fd7b3e411d567fa83037
Nikita Gudin <nikitagudin.ads@gmail.com>
2023-07-22 20:39:18 +0300

    test heroku

--- js/app.js
+++ js/app.js
@@ -2,4 +2,19 @@
  Entry point of app
  */
 
-console.log('hello world');
+ const formMessage = document.querySelector('.form__message');
+
+ formMessage.addEventListener('submit', (e) => {
+   e.preventDefault();
+ 
+   fetch('http://localhost:7070/')
+     .then(response => response.json())
+     .then(data => {
+       console.log(data);
+     })
+     .catch(error => {
+       console.error('Ошибка при выполнении запроса', error);
+     });
+ });
+ 
+ 
